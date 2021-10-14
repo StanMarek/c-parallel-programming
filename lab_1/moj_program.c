4ca3bf9d56ea931f4a31de847a5d0c15fe3f61ca
StanMarek <stanislawmarek21@gmail.com>
2021-10-14 22:25:39 +0200

    init lab 2

--- lab_1/moj_program.c
+++ lab_1/moj_program.c
@@ -1,6 +1,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<time.h>
+#include "pomiar_czasu.h"
  
 const int liczba = 100000;
 
@@ -10,25 +11,30 @@ main(){
   int i,j,k,l;
 
   k = -50000;
+  inicjuj_czas();
   for(i=0;i<liczba;i++){
 
     printf("%d ",k+i);
 
   }
   printf("\n");
-
   printf("Czas wykonania %d operacji wejscia/wyjscia: \n",liczba);
-
+  drukuj_czas();
 
   a = 1.000001;
-  for(i=0;i<liczba;i++){
+  double t1 = czas_zegara();
+  double t2 = czas_CPU();
+  for(i=0;i<liczba*100;i++){
 
     a = 1.000001*a+0.000001; 
 
   }
+  t1 = czas_zegara() - t1;
+  t2 = czas_CPU() - t2;
 
-  printf("Wynik operacji arytmetycznych: %lf\n", a);
-  printf("Czas wykonania %d operacji arytmetycznych: \n",liczba);
-
+  printf("\nWynik operacji arytmetycznych: %lf\n", a);
+  printf("Czas wykonania %d operacji arytmetycznych: \n",liczba*100);
+  printf("Czas zegara: %f\n", t1);
+  printf("Czas CPU: %f\n", t2);
 
 }
